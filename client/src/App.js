cea8c824044657b51e59b044b2c20529f2656607
Karsten 7 <Karsten7@users.noreply.github.com>
2018-10-25 19:04:04 +0200

    Change App title

--- client/src/App.js
+++ client/src/App.js
@@ -12,14 +12,14 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator</h1>
+            <h1 className="App-title">Fib Calculator V2 </h1>
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
-            <div>
-              <Route exact path="/" component={Fib} />
-              <Route path="/otherpage" component={OtherPage} />
-            </div>
           </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
       </Router>
     );
